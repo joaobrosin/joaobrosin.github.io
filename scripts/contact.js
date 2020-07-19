b18fc9b3937b879d7d2673f4ffc64381e6181674
João Brosin <joao.brosin@gmail.com>
2020-07-18 21:21:13 -0300

    script of contact interaction fixed

--- scripts/contact.js
+++ scripts/contact.js
@@ -6,16 +6,20 @@ const iconImage = document.querySelector('.icons img')
 
 instagramButton.addEventListener('mouseover', function() {
     iconImage.src = 'assets/instagram-icon.svg'
+
 })
 
 emailButton.addEventListener('mouseover', function() {
     iconImage.src = 'assets/email-icon.svg'
+
 })
 
 githubButton.addEventListener('mouseover', function() {
     iconImage.src = 'assets/github-icon.svg'
+
 })
 
 linkedinButton.addEventListener('mouseover', function() {
     iconImage.src = 'assets/linkedin-icon.svg'
+    
 })
